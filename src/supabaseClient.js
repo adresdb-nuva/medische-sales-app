88edc09a9d31dfa2f1007f4a664424a79227c672
adresdb-nuva <adres.db@gmail.com>
2026-03-02 18:03:33 +0100

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,8 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-// We halen de URL en Key uit de omgevingsvariabelen (veiligere methode)
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'JOUW_SUPABASE_PROJECT_URL'
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'JOUW_SUPABASE_ANON_KEY'
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 
-// De client wordt hier één keer aangemaakt en geëxporteerd
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
