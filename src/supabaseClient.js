34887eb79026cd99815f9ba6e5ceea908c6d486a
adresdb-nuva <adres.db@gmail.com>
2026-03-02 19:49:52 +0100

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -2,6 +2,6 @@ import { createClient } from '@supabase/supabase-js'
 
 // PLAK HIER JE EIGEN GEGEVENS UIT HET SUPABASE DASHBOARD (Settings > API)
 const supabaseUrl = 'https://jffnfkloolypvdrnxsvg.supabase.co'
-const supabaseAnonKey = 'sb_publishable_qLNyJAt6dph5Kd-atQ051A_MaRU5eKL'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpmZm5ma2xvb2x5cHZkcm54c3ZnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI0NjU3NjksImV4cCI6MjA4ODA0MTc2OX0.T-_cwIcConpqyN7FOefCjk9ZByo5QigyDDn7CuHlGng'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
